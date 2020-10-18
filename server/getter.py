7891780449d0037c04be3db5d7621c8e42a52dbd
Eric Mesina <esmesina02@gmail.com>
2020-10-18 09:46:40 -0400

    :)

--- server/getter.py
+++ server/getter.py
@@ -3,19 +3,25 @@ import numpy as np
 import datetime as dt
 
 def get(date, time):
-    hours = (date.month * 30 + date.day - 1) * 24 + 1
+    start = dt.date(2020, 8, 30)
+    delta = date - start
+    hours = delta.days * 24 
 
-    total_data = pd.read_csv("total_data.csv")
-    average = total_data.mean()
-    target = total_data[hours - 1]
+    total_data = pd.read_csv("predictions.csv")
+    predicted_series = pd.Series(total_data.iloc[:,0])
+    average = total_data.iloc[:,0].mean()
+    target = predicted_series[hours - 1]
     percent = 100 * (target / average)
 
     upper = np.percentile(total_data, 65)
     lower = np.percentile(total_data, 35)
 
     if target < lower:
-        return "Traffic levels in Atlanta will be low, about %d percent of the average" % percent
+        return "The predicted traffic level is %d, about %d percent of the average traffic level of %d. \n" \
+              "Traffic levels in Atlanta will be low." % (target, percent, average)
     elif target > upper:
-        return "Traffic levels in Atlanta will be high, about %d percent of the average" % percent
+        return "The predicted traffic level is %d, about %d percent of the average traffic level of %d. \n" \
+              "Traffic levels in Atlanta will be high." % (target, percent, average)
     else:
-        return "Traffic levels in Atlanta will be moderate, about %d percent of the average" % percent
+        return "The predicted traffic level is %d, about %d percent of the average traffic level of %d. \n" \
+              "Traffic levels in Atlanta will be moderate." % (target, percent, average)
